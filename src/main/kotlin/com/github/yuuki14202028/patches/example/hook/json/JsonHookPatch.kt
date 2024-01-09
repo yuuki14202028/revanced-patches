98a2270352de0768ac3e892b76e5257e1bc71425
yuuki14202028 <yuuki14202028@gmail.com>
2024-01-09 23:01:54 +0900

    feat: debug

--- src/main/kotlin/com/github/yuuki14202028/patches/example/hook/json/JsonHookPatch.kt
+++ src/main/kotlin/com/github/yuuki14202028/patches/example/hook/json/JsonHookPatch.kt
@@ -88,7 +88,7 @@ object JsonHookPatch : BytecodePatch(
                     ) throw InvalidClassException(classDef.type, "Not a hook class")
 
                 }
-            } ?: throw ClassNotFoundException("Failed to find hook class in ${context.classes.map { c -> c.type }}")
+            } ?: throw ClassNotFoundException("Failed to find hook class in ${context.classes.filter { it.type.startsWith("Lapp/revanced/twitter/patches/hook/patch") }.map { c -> c.type }}")
         }
     }
 
